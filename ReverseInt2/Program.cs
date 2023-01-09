d98232474d15aebeb57a78102d55c70fd9e98fa9
Yzybk <yusuf_zeybek@hotmail.com>
2023-01-09 10:33:13 +0300

    Add project files.

---
+++ ReverseInt2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try
+            {
+
+                Console.WriteLine("Please provide input number");
+
+                int input = int.Parse(Console.ReadLine());
+                int backUp = input;
+                int remainder, reverse = 0;
+
+                while (input > 0)
+                {
+                    remainder = input % 10;
+                    reverse = reverse * 10 + remainder;
+                    input = input / 10;
+
+                }
+
+                Console.WriteLine($"{backUp} Reverse number is {reverse}");
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+
+}
